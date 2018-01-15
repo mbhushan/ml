f237608ed7779e75e5ca62da89c476ca40688f6e
Mani Bhushan <manibhushan.cs@gmail.com>
2018-01-14 16:20:47 -0800

    stochastic gradient descent - intial commit and example

--- logistic_regression/logistic_reg.py
+++ logistic_regression/logistic_reg.py
@@ -9,6 +9,39 @@ def predict(row, coefficients):
     return 1 / (1.0 + exp(-yhat))
 
 
+# Estimate logistic regression coefficients using stochastic gradient descent
+def coefficients_sgd(train, l_rate, n_epoch):
+    coef = [0.0 for i in range(len(train[0]))]
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            yhat = predict(row, coef)
+            error = row[-1] - yhat
+            sum_error += error ** 2
+            coef[0] += l_rate * error * yhat * (1.0 - yhat)
+            for i in range(len(row) - 1):
+                coef[i + 1] += l_rate * error * yhat * (1.0 - yhat) * row[i]
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+    return coef
+
+
+def calc_coefficients():
+    # Calculate coefficients
+    dataset = [[2.7810836, 2.550537003, 0],
+               [1.465489372, 2.362125076, 0],
+               [3.396561688, 4.400293529, 0],
+               [1.38807019, 1.850220317, 0],
+               [3.06407232, 3.005305973, 0],
+               [7.627531214, 2.759262235, 1],
+               [5.332441248, 2.088626775, 1],
+               [6.922596716, 1.77106367, 1],
+               [8.675418651, -0.242068655, 1],
+               [7.673756466, 3.508563011, 1]]
+    l_rate = 0.9
+    n_epoch = 100
+    coef = coefficients_sgd(dataset, l_rate, n_epoch)
+    print(coef)
+
 def test_predictions():
     dataset = [[2.7810836, 2.550537003, 0],
                [1.465489372, 2.362125076, 0],
@@ -28,6 +61,7 @@ def test_predictions():
 
 def main():
     test_predictions()
+    calc_coefficients()
 
 if __name__ == '__main__':
     main()
